f5b7c4c0bbbb02a11d4bd9ff9c970533e0004a83
Fernando Lugo <fernandolugo2-02@hotmail.com>
2025-09-18 01:42:07 -0600

    Configuracion del proyecto

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,17 +5,34 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import PrimeVue from 'primevue/config'; // ✅ Importa PrimeVue
+import 'primevue/resources/themes/saga-blue/theme.css'; // ✅ funciona en v3
+
+
+import 'primevue/resources/primevue.min.css'; // Estilos base
+import 'primeicons/primeicons.css'; // Iconos
+import FloatLabel from 'primevue/floatlabel';
+import InputText from 'primevue/inputtext';
+
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    resolve: (name) =>
+        resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
-        return createApp({ render: () => h(App, props) })
-            .use(plugin)
-            .use(ZiggyVue)
-            .mount(el);
+        const app = createApp({ render: () => h(App, props) });
+
+        app.use(plugin);        // Inertia
+        app.use(ZiggyVue);      // Ziggy
+        app.use(PrimeVue);      // ✅ PrimeVue
+
+        //Componentes globales
+        app.component('FloatLabel', FloatLabel);
+        app.component('InputText', InputText);
+
+        return app.mount(el);
     },
     progress: {
         color: '#4B5563',
